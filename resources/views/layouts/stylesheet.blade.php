4321c494f67854a07734d7059d337fea09eedae3
Faisalhfdz <faisalhfdz25@gmail.com>
2022-05-24 19:59:51 +0700

    first commit

---
+++ resources/views/layouts/stylesheet.blade.php
@@ -0,0 +1,17 @@
+  <!--====== Favicon Icon ======-->
+  <link rel="shortcut icon" href="assets/images/mardira.png" type="image/png">
+
+  <!--====== Bootstrap css ======-->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+  <!--====== Line Icons css ======-->
+  <link rel="stylesheet" href="assets/css/LineIcons.css">
+
+  <!--====== Magnific Popup css ======-->
+  <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+  <!--====== Default css ======-->
+  <link rel="stylesheet" href="assets/css/default.css">
+
+  <!--====== Style css ======-->
+  <link rel="stylesheet" href="assets/css/style.css">
